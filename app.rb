8db9f7700e495716caaf4ed1b4c9c727e7bb8284
Ritav Das <das.2@dublinstudents.net>
2024-06-21 09:16:27 -0400

    complete blender project

--- app.rb
+++ app.rb
@@ -18,6 +18,59 @@ class App < Sinatra::Base
   def title
     'Ritavs Blender App'
   end
+  
+  def accounts
+      Process.groups.map do |gid|
+        Etc.getgrgid(gid).name
+      end.select do |group|
+        group.start_with?('P')
+      end
+  end
+  
+  def blend_files
+    Dir.glob("#{__dir__}/blend_files/*.blend")
+  end
+  
+  def images(directory)
+      Dir.glob("#{directory}/*.png")
+  end
+  
+  post '/render/frames' do
+      
+      walltime = format('%02d:00:00', params[:walltime])
+      exports = "BLEND_FILE_PATH=#{params[:blend_file]}"
+      exports = "#{exports},OUTPUT_DIR=#{params[:project_directory]}"
+      exports = "#{exports},FRAME_RANGE=#{params[:frame_range]}"
+      
+      
+      args =['-A', params[:account], '-n', params[:num_cpus]]
+      args.concat(['-t', walltime, '-M', 'pitzer', '--parsable'])
+      args.concat(['--export', exports])
+      
+      script = "#{__dir__}/scripts/render_frames.sh"
+      
+      output = `/bin/sbatch #{args.join(' ')} #{script} 2>&1`
+      session[:flash] = { info: "Submitted job with id '#{output.split(';').first}'" }
+     
+     redirect(url("/projects/#{File.basename(params[:project_directory])}"))
+  end
+
+     post '/render/video' do
+        walltime = format('%02d:00:00', params[:walltime])
+        script = "#{__dir__}/scripts/render_video.sh"
+        exports = "FRAMES_PER_SEC=#{params[:frames_per_second]}"
+        exports = "#{exports},OUTPUT_DIR=#{params[:project_directory]}"
+        
+        args = ['-A', params[:account], '-n', params[:num_cpus]]
+        args.concat(['-M', 'pitzer', '--parsable'])
+        args.concat(['-t', walltime, '--export', exports])
+        
+        output = `/bin/sbatch #{args.join(' ')} #{script} 2>&1`
+        
+        session[:flash] = { info: "Submitting job with output: #{output}" }
+        redirect(url("/projects/#{File.basename(params[:project_directory])}"))
+    end
+        
 
   get '/examples' do
     erb(:examples)
@@ -28,17 +81,44 @@ class App < Sinatra::Base
     @flash = session.delete(:flash) || { info: 'Welcome to Summer Institute!' }
     erb(:index)
   end
-end
-
-# in app.rb
-class App
-  get '/projects/new' do
-    erb(:new_project)
+  
+  get '/projects/:name' do
+    if params[:name] == 'new'
+        erb(:new_project)
+    else
+        @directory = Pathname.new("#{projects_root}/#{params[:name]}")
+        @flash = session.delete(:flash)
+        @images = images(@directory)
+        
+        if(@directory.exist? && @directory.directory?)
+            erb(:show_project)
+        else
+            session[:flash] = { danger: "There is no project named '#{params[:name]}'."}
+            redirect(url("/"))
+        end
+    end
   end
-end
+  
+  def sanitize_project_name(name)
+    name.downcase.gsub(' ', '_')
+  end
 
-class App
+  def projects_root
+    "#{__dir__}/projects"
+  end
+
+  def projects
+      Dir.children(projects_root).select do |path|
+          Pathname.new("#{projects_root}/#{path}").directory?
+      end.sort_by(&:to_s)
+  end
+
   post '/projects/new' do
-    redirect(url("/projects/new"))
+    directory_name = sanitize_project_name(params[:name])
+    
+    FileUtils.mkdir_p("#{projects_root}/#{directory_name}")
+    session[:flash] = { info: "Created project '#{params[:name]}'."}
+    redirect(url("/"))
   end
 end
+
--- public/app.js
+++ public/app.js
@@ -1,4 +1,70 @@
 
-jQuery(() => {
-  console.log('hello world');
-});
+
+window.onload = (event) => {
+  updateCarousel();
+};
+
+function updateCarousel() {
+    console.log('hello from my new function');
+    
+    const configElement = document.getElementById('project_config')
+    if(configElement === null) {
+        return;
+    }
+    
+    const directory = configElement.dataset.directory;
+    console.log(`looking for images in ${directory}`);
+    
+    const url = `/pun/sys/dashboard/files/fs/${directory}`;
+    const options = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+    
+    fetch(url, options)
+        .then((response) => {
+            console.log(`Fetch resolved at ${new Date().toISOString()}.`);
+            return response;
+        }).then((response) => response.json())
+          .then((json) => json['files'])
+          .then ((files) => files.map((file) => file['name']))
+          .then((files) => files.filter((file) => file.endsWith('png')))
+          .then((files) => files.splice(0, 9))
+          .then((files) => {
+              
+              for(const file of files) {
+                  const image = document.getElementById(file);
+                  
+                  if(image !== null) {
+                      console.log(`Skipping ${file} because it's already on the page`);
+                      continue;
+                  }
+                  
+                  console.log(`Adding ${file} to the DOM.`);
+                  
+                  const imageUrl = `/pun/sys/dashboard/files/fs/${directory}/${file}`;
+                  const newImage = document.createElement('div');
+                  newImage.classList.add('carousel-item');
+                  newImage.innerHTML = `<img class="d-block w-100" src="${imageUrl}">`;
+                  newImage.id = file;
+                  
+                  const carousel = document.getElementById('image_carousel_inner');
+                  carousel.append(newImage);
+                  
+                  const indicators = document.getElementById('image_carousel_indicators');
+                  const slideTo = indicators.children.length;
+                  
+                  const newIndicator = document.createElement('li');
+                  newIndicator.setAttribute('data-target', '#image_carousel');
+                  newIndicator.setAttribute('data-slide-to', slideTo);
+                  
+                  indicators.append(newIndicator);
+              }
+              
+          });
+    
+    console.log(`Fetch called at ${new Date().toISOString()}.`);
+    setTimeout(updateCarousel, 10000);
+    
+}
